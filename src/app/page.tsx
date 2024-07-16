10f996be92accb160c3601c1a59b2d17bf79162a
sseung95 <seung3657@gmail.com>
2024-07-16 15:31:55 +0900

    update: 메인 페이지에 각 페이지 링크 이동 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>HOME</div>;
+  return (
+    <div className="flex flex-col gap-24 h-screen bg-black p-20">
+      <div className="flex flex-col gap-8">
+        <h1 className="text-2xl font-medium text-slate-900 dark:text-slate-200">Framer Motion Practice</h1>
+        <div className="flex items-start gap-4">
+          <LinkButton label="animationControls" link="/tutorial/animationControls" />
+          <LinkButton label="basic show/hide" link="/tutorial/basic" />
+          <LinkButton label="gestures" link="/tutorial/gestures" />
+          <LinkButton label="inView" link="/tutorial/inView" />
+          <LinkButton label="page scroll percent" link="/tutorial/scroll" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-8">
+        <h1 className="text-2xl font-medium text-slate-900 dark:text-slate-200">Framer Motion Components</h1>
+        <div className="flex items-start gap-4">
+          <LinkButton label="bubble text" link="/tutorial/bubbleText" />
+          <LinkButton label="spotlight button" link="/tutorial/button" />
+          <LinkButton label="horizontal scroll" link="/tutorial/horizontalScroll" />
+        </div>
+      </div>
+    </div>
+  );
 }
+
+const LinkButton = ({ label = '', link = '#' }) => {
+  return (
+    <Link
+      href={link}
+      className="group relative overflow-hidden rounded-lg border-[1px] border-neutral-500 bg-neutral-700 px-4 py-2 font-mono font-medium uppercase text-neutral-300 transition-colors hover:text-indigo-300"
+    >
+      {label}
+    </Link>
+  );
+};
