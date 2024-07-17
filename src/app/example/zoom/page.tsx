1d886fd90ee8b63dd5a66b98431780c03f66d214
sseung95 <seung3657@gmail.com>
2024-07-17 11:42:14 +0900

    update: scroll zoom 로직 수정
    - zoom out 되는 부분 useScroll 사용하도록 변경
    - 배경 커지는 부분 scale 조정하도록 변경

--- src/app/example/zoom/page.tsx
+++ src/app/example/zoom/page.tsx
@@ -1,10 +1,11 @@
 'use client';
+import { useRef } from 'react';
 import { motion, useInView, useScroll, useTransform } from 'framer-motion';
-import { useEffect, useRef } from 'react';
 
 const page = () => {
   const zoomRef = useRef(null);
-  const isInViewZoomRef = useInView(zoomRef, { amount: 0.5 });
+  const { scrollYProgress: zoomScrollYProgress } = useScroll({ target: zoomRef });
+  const zoomScale = useTransform(zoomScrollYProgress, [0, 0.5], [1, 0.8]);
 
   const bgRef = useRef(null);
   const isInViewBgRef = useInView(bgRef, { amount: 0.5 });
@@ -15,8 +16,8 @@ const page = () => {
   const isExpansion = isInViewBgRef && !isInViewNextRef;
 
   return (
-    <>
-      <section className="flex items-center justify-center h-[100vh] bg-slate-200 ">
+    <div className="relative">
+      <section className="relative flex items-center justify-center h-[100vh] bg-slate-200">
         <p className="text-black text-4xl font-bold">SCROLL</p>
       </section>
       <section className="h-[100vh] bg-slate-200 "></section>
@@ -25,36 +26,42 @@ const page = () => {
       <section ref={zoomRef} className="h-[150vh]">
         <motion.div
           className="h-[100vh] bg-lime-100 sticky top-0"
-          initial={{ scale: 1 }}
-          animate={{ scale: isInViewZoomRef ? 0.8 : 1 }}
+          style={{ scale: zoomScale }}
           transition={{ duration: 0.25 }}
         ></motion.div>
       </section>
-      <section className="h-[200vh] bg-slate-400"></section>
 
-      {/* 배경 100vh, 100vw 로 변하는 섹션 */}
-      <section className="h-[150vh] flex justify-center">
+      <section className="relative h-[200vh] bg-slate-400">
+        <p>안녕하세요</p>
         <motion.div
-          ref={bgRef}
-          className="h-[70vh] w-[80vw] bg-black flex justify-center pt-[200px] sticky top-0"
-          initial={{ width: '70vw', height: '70vh' }}
-          animate={{ width: isExpansion ? '100vw' : '70vw', height: isExpansion ? '100vh' : '70vh' }}
-          transition={{ duration: 0.25 }}
-        >
+          className="absolute h-[100%] w-[100%] bg-black"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: isExpansion ? 0.3 : 0 }}
+        ></motion.div>
+      </section>
+
+      {/* 배경 크기 변하는 섹션 */}
+      <section className="relative h-[100vh] flex justify-center mt-[128px] mb-[128px]">
+        <div ref={bgRef} className="absolute top-0">
           <motion.div
-            className="h-[400px] w-[400px] rounded-[32px] text-white flex justify-center items-center bg-slate-800"
-            initial={{ scale: 0.9 }}
-            animate={{ scale: isExpansion ? 1 : 0.8 }}
-            transition={{ duration: 0.35 }}
-          >
-            안녕하세요
-          </motion.div>
-        </motion.div>
+            className="h-[100vh] w-[1100px] bg-orange-200 rounded-[38px]"
+            initial={{ scale: 1 }}
+            animate={{ scale: isExpansion ? 1.5 : 1 }}
+            transition={{
+              type: 'tween',
+            }}
+          ></motion.div>
+        </div>
+        <div className="relative mt-[50px]">
+          <p>안녕하세요</p>
+        </div>
       </section>
 
       <section className="h-[50vh] bg-slate-400"></section>
       <section ref={nextRef} className="h-[50vh] bg-red-100"></section>
-    </>
+
+      <div className="absolute top-0 left-0 h-[100%] w-[100%]"></div>
+    </div>
   );
 };
 
